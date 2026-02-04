287a57d8a3e28bfb03d62a69b1b52175f022cd2f
KadenL9 <kaden.r.luu@gmail.com>
2026-02-03 21:04:27 -0800

    graph displays

--- js/activities.js
+++ js/activities.js
@@ -6,7 +6,12 @@ function parseTweets(runkeeper_tweets) {
 	}
 	
 	tweet_array = runkeeper_tweets.map(function(tweet) {
-		return new Tweet(tweet.text, tweet.created_at);
+		let t = new Tweet(tweet.text, tweet.created_at);
+		return {
+			time: t.time,
+			distance: t.distance,
+			activityType: t.activityType
+		}
 	});
 
 	let activityCount = new Map();
@@ -88,103 +93,80 @@ function parseTweets(runkeeper_tweets) {
 
 	document.getElementById("weekdayOrWeekendLonger").innerText = dayAverage[0][0];
 
-	//TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
-
-	/*activity_vis_spec = {
+	
+	// plot of activities and their counts
+	activity_vis_spec = {
 	  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
 	  "description": "A graph of the number of Tweets containing each type of activity.",
 	  "data": {
 	    "values": tweet_array
+	  },
+	  "mark": "bar",
+	  "encoding": {
+		"x": {
+			"field": "activityType",
+			"type": "nominal",
+			"axis": {"title": "Activity Type"}
+		},
+		"y": {
+			"aggregate": "count",
+			"type": "quantitative",
+			"axis": {"title": "Count"}
+		}
 	  }
-	  //TODO: Add mark and encoding
 	};
 	vegaEmbed('#activityVis', activity_vis_spec, {actions:false});
-	*/
-	//TODO: create the visualizations which group the three most-tweeted activities by the day of the week.
-	//Use those visualizations to answer the questions about which activities tended to be longest and when.
+
+	let isAggregated = false;
+	renderGraph(tweet_array, activityArray, isAggregated);
+
+	document.getElementById("aggregate").addEventListener("click", function() {
+		isAggregated = !isAggregated;
+		
+		this.innerText = isAggregated ? "Show all activities" : "Show means";
+
+		renderGraph(tweet_array, activityArray, isAggregated);
+	});
 }
 
-
-/*function activityInfo(tweet_array) {
-	let activityCount = new Map();
-	let activityTotal = new Map();
-
-	tweet_array.forEach((tweet) => {
-		let activity = tweet.activityType;
-		let distance = tweet.distance;
-		
-		if (activity === "unknown") {
-			return;
+function renderGraph(tweet_array, activityArray, isAggregated) {
+	distance_vis_agg_spec = {
+		"$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+		"description": "Average distances by day of the week for top 3 activities",
+		"data": { "values": tweet_array },
+		"transform": [
+			{
+				"filter": {
+					"field": "activityType",
+					"oneOf": [activityArray[0][0], activityArray[1][0], activityArray[2][0]]
+				}
+			}
+		],
+		"mark": "point",
+		"encoding": {
+			"x": {
+				"field": "time",
+				"type": "temporal",
+				"timeUnit": "day",
+				"sort": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+				"axis": {"title": "Day of Week"}
+			},
+			"y": {
+				"field": "distance",
+				"aggregate": isAggregated ? "average" : undefined,
+				"type": "quantitative",
+				"axis": {"title": "Average Distance (mi)"}
+			},
+			"color": {
+				"field": "activityType",
+				"type": "nominal",
+				"legend": {"title": "Activity Type"}
+			}
 		}
+	};
+	vegaEmbed("#distanceVisAggregated", distance_vis_agg_spec, {actions:false});
+}
 
-		if (activityCount.has(activity)) {
-			activityCount.set(activity, activityCount.get(activity) + 1);
-			activityTotal.set(activity, activityTotal.get(activity) + distance); 
-		}
-		else {
-			activityCount.set(activity, 1);
-			activityTotal.set(activity, distance);
-		}
-	});
-
-	document.getElementById("numberActivities").innerText = activityCount.size;
-
-	// display 3 most common activities
-	let activityArray = Array.from(activityCount.entries());
-	activityArray.sort((a, b) => b[1] - a[1]);
-
-	document.getElementById("firstMost").innerText = activityArray[0][0];
-	document.getElementById("secondMost").innerText = activityArray[1][0];
-	document.getElementById("thirdMost").innerText = activityArray[2][0];
-
-	// display information about longest/shortest activity
-	let averages = new Map();
-	activityTotal.forEach((totalDistance, activityName) => {
-		let count = activityCount.get(activityName);
-		averages.set(activityName, totalDistance / count);
-	});
-
-	// display longest/shortest activities
-	let activityAverage = Array.from(averages.entries());
-	activityAverage.sort((a, b) => b[1] - a[1]);
-
-	document.getElementById("longestActivityType").innerText = activityAverage[0][0];
-	document.getElementById("shortestActivityType").innerText = activityAverage[activityAverage.length - 1][0];
-
-	// display day of longest activities
-	let activityDayCount = new Map();
-	let activityDayTotal = new Map();
-
-	activityDayCount.set("weekdays", 0);
-    activityDayCount.set("weekends", 0);
-    activityDayTotal.set("weekdays", 0);
-    activityDayTotal.set("weekends", 0);
-
-	tweet_array.forEach((tweet) => {
-		if (tweet.activityType === "unknown") {
-			return;
-		}
-
-		let day = tweet.time.getDay();
-		let category = (day === 0 || day === 6) ? "weekends" : "weekdays";
-		let activityDist = tweet.distance;
-
-		activityDayCount.set(category, activityDayCount.get(category) + 1);
-		activityDayTotal.set(category, activityDayTotal.get(category) + activityDist);
-
-	});
-
-	let dayAverages = new Map();
-	activityDayTotal.forEach((totalDistance, category) => {
-		let count = activityDayCount.get(category);
-		dayAverages.set(category, totalDistance / count);
-	});
-
-	let dayAverage = Array.from(dayAverages.entries());
-	dayAverage.sort((a, b) => b[1] - a[1]);
-
-	document.getElementById("weekdayOrWeekendLonger").innerText = dayAverage[0][0];
-}*/
 
 //Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function (event) {
